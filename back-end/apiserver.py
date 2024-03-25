c6d18d0cf160955c4c98a31c9ec5a2420f09ab96
Jeevithan Mahenthran <JeevithanMahenthran@gmail.com>
2024-03-24 22:39:45 -0700

    added error handlers for api request

--- back-end/apiserver.py
+++ back-end/apiserver.py
@@ -2,25 +2,45 @@ import sys
 import requests
 import json
 
+# function to handle errors
+def handle_error(response):
+    status_code = response.status_code
+    if status_code == 200:
+        return 'OK'
+    if status_code == 404:
+        return "Summoner name and tag don't exist, please try again"
+    elif status_code in [401, 403]:
+        return "API Key is expired!"
+    else:
+        return "An unexpected error occurred"
+
+# function to get icon from that puuid
+def getPlayerIcon(request, puuid, key):
+    new_request = request + puuid + '?api_key=' + key
+    return requests.get(new_request)
+
 summoner, tag = sys.argv[1], sys.argv[2]
-api_key = 'RGAPI-a7fd8aac-f961-42f1-b24a-af73ff7afa0b'
+api_key = 'RGAPI-b8f2c841-61f3-4f85-b525-1a1b00b43911'
 accountRequest = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + summoner + '/' + tag + '?api_key=' + api_key
 summonerRequest = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/'
 
 # getting summoner puuid from summoner name and tag
 accountData = requests.get(accountRequest)
+if handle_error(accountData) != 'OK':
+    print(handle_error(accountData))
+    sys.exit()
+
 playerInfo = accountData.json()
 puuid = playerInfo['puuid']
-print("puuid: " + puuid)
 
-# function to get icon from that puuid
-def getPlayerIcon(request, puuid):
-    new_request = request + puuid + '?api_key=' + api_key
-    return requests.get(new_request)
+# getting icon from puuid 
+summonerData = getPlayerIcon(summonerRequest, puuid, api_key)
+if handle_error(summonerData) != 'OK':
+    print(handle_error(summonerData))
+    sys.exit()
+  
+playerInfo = summonerData.json()
+iconID = playerInfo['profileIconId'] 
 
-iconRequest = getPlayerIcon(summonerRequest, puuid)
-summonerData = iconRequest.json()
-iconID = summonerData['profileIconId'] 
-print("iconID: " + str(iconID))
-
+print({"puuid": puuid, "iconID": str(iconID)})
 sys.stdout.flush()
--- front-end/script.js
+++ front-end/script.js
@@ -1,13 +1,16 @@
 document.getElementById('searchButton').addEventListener('click', async function() {
     const summonerName = document.getElementById('summonerName').value;
     const summonerTag = document.getElementById('summonerTag').value;
-
-    const url = `/client-search?summonerName=${summonerName}&summonerTag=${summonerTag}`; // location of httpserver.js
+    if (summonerName.trim() === "" || summonerTag.trim() === "") {
+        document.getElementById('tempOutput').innerText = "Please enter a summoner name and tag.";
+        return;
+    }
+    const url = `/client-search?summonerName=${encodeURIComponent(summonerName)}&summonerTag=${encodeURIComponent(summonerTag)}`; // location of httpserver.js
 
     console.log("(HTML) Clicked! Sending and Receiving Data Now...");
     const response = await fetch(url); // sending GET request to client.js
     const data = await response.text();
     document.getElementById('tempOutput').innerText = data; // displaying the returned data on front end
     console.log("(HTML) Logging data from JavaScript into website console:");
-    console.log(data[1]);
+    console.log(data);
 });
