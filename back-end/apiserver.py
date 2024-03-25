396fbd16718c28778b022feb29438d29e62f9925
Jeevithan Mahenthran <JeevithanMahenthran@gmail.com>
2024-03-25 15:36:45 -0700

    added object.json being passed to js instead of string

--- back-end/apiserver.py
+++ back-end/apiserver.py
@@ -19,6 +19,7 @@ def getPlayerIcon(request, puuid, key):
     new_request = request + puuid + '?api_key=' + key
     return requests.get(new_request)
 
+SENDING_DATA_HASHMAP = {}
 summoner, tag = sys.argv[1], sys.argv[2]
 api_key = 'RGAPI-b8f2c841-61f3-4f85-b525-1a1b00b43911'
 accountRequest = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + summoner + '/' + tag + '?api_key=' + api_key
@@ -32,6 +33,7 @@ if handle_error(accountData) != 'OK':
 
 playerInfo = accountData.json()
 puuid = playerInfo['puuid']
+SENDING_DATA_HASHMAP['puuid'] = puuid
 
 # getting icon from puuid 
 summonerData = getPlayerIcon(summonerRequest, puuid, api_key)
@@ -41,6 +43,9 @@ if handle_error(summonerData) != 'OK':
   
 playerInfo = summonerData.json()
 iconID = playerInfo['profileIconId'] 
+SENDING_DATA_HASHMAP['iconID'] = iconID
 
-print({"puuid": puuid, "iconID": str(iconID)})
+json_data = json.dumps(SENDING_DATA_HASHMAP)
+print(json_data)
+
 sys.stdout.flush()
--- front-end/script.js
+++ front-end/script.js
@@ -1,5 +1,3 @@
-
-
 document.getElementById('searchButton').addEventListener('click', async function() {
     const summonerName = document.getElementById('summonerName').value;
     const summonerTag = document.getElementById('summonerTag').value;
@@ -9,15 +7,20 @@ document.getElementById('searchButton').addEventListener('click', async function
     }    
 
     //Testing that change paging works, should be called at the end of the method
-    changePage('stats.html',summonerName,summonerTag);
+    //changePage('stats.html',summonerName,summonerTag);
 
     const url = `/client-search?summonerName=${encodeURIComponent(summonerName)}&summonerTag=${encodeURIComponent(summonerTag)}`; // location of httpserver.js
     console.log("(HTML) Clicked! Sending and Receiving Data Now...");
     const response = await fetch(url); // sending GET request to client.js
     const data = await response.text();
-    document.getElementById('tempOutput').innerText = data; // displaying the returned data on front end
+
+    const parsedHashmap = JSON.parse(data);
     console.log("(HTML) Logging data from JavaScript into website console:");
-    console.log(data);
+    console.log(parsedHashmap);
+    const iconID = parsedHashmap.iconID
+    const puuID = parsedHashmap.puuid
+    console.log("(HTML) parsed iconID and puuid: ", puuID, iconID);
+    document.getElementById('tempOutput').innerText = data; // displaying the returned data on front end
 
     //Switch page at the end of getting data... 
     // changePage('stats.html',summonerName,summonerTag,data);
