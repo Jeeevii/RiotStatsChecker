8a34af8a412ad7ce85161ede438990ca80c74a5a
Jeevithan Mahenthran <JeevithanMahenthran@gmail.com>
2024-03-25 19:44:22 -0700

    stashing

--- back-end/apiserver.py
+++ back-end/apiserver.py
@@ -39,6 +39,16 @@ def getPlayerData(request, puuid, key):
         sys.exit()
     return accountData.json()
 
+# helper for getting player mastery from puuid
+def getMasteryData(request, puuid, key):
+    new_request = request + puuid + '?api_key=' + key
+    accountData = requests.get(new_request)
+    error = handle_error(accountData)
+    if error:
+        print(error)
+        sys.exit()
+    return accountData.json()
+
 # function to get all player rank info
 def getPlayerRank(request, id, key):
     new_request = request + id + '?api_key=' + key
@@ -52,12 +62,13 @@ def getPlayerRank(request, id, key):
 #================================================================================================
 # starter variables
 HASHMAP_DATA = {}
-#summoner, tag = 'Gaia', 'memo'
-summoner, tag = sys.argv[1], sys.argv[2]
+summoner, tag = 'Jeevi', '0001'
+#summoner, tag = sys.argv[1], sys.argv[2]
 api_key = 'RGAPI-b8f2c841-61f3-4f85-b525-1a1b00b43911'
 accountRequest = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/'
 summonerRequest = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/'
 leagueRequest = 'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/'
+masteryRequest = 'https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/'
 
 # getting summoner puuid from summoner name and tag
 accountData = getSummonerData(accountRequest, summoner, tag, api_key)
@@ -69,6 +80,17 @@ tagLine = accountData['tagLine']
 summonerData = getPlayerData(summonerRequest, puuid, api_key)
 iconID = summonerData['profileIconId']
 summonerID = summonerData['id']
+
+# getting top 3 mastery champs and their mastery points from puuid
+masteryData = getMasteryData(masteryRequest, puuid, api_key) # WARMING: Data is HUGE
+
+championID_first = 'N/A'
+championID_first_mastery = 'N\A'
+championID_second = 'N/A'
+championID_third = 'N/A'
+for i in range(3): # top 3 champs
+    print(masteryData[i])
+
 
 #get rank and tier from id
 leagueData = getPlayerRank(leagueRequest, summonerID, api_key)
@@ -99,14 +121,15 @@ for i in range(len(leagueData)):
 HASHMAP_DATA['gameName'] = gameName
 HASHMAP_DATA['tagLine'] = tagLine
 HASHMAP_DATA['iconID'] = iconID
-# rank data 
+# rank solo data 
 HASHMAP_DATA['soloTier'] = soloTier
 HASHMAP_DATA['soloRank'] = soloRank
 HASHMAP_DATA['soloLP'] = soloLP
+#rank flex data
 HASHMAP_DATA['flexTier'] = flexTier
 HASHMAP_DATA['flexRank'] = flexRank
 HASHMAP_DATA['flexLP'] = flexLP
 
 json_data = json.dumps(HASHMAP_DATA) # converts data into json file to send
-print(json_data)
+#print(json_data)
 sys.stdout.flush()
