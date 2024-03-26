4ba2c035ef2159ea0be71cd3790bd13910343515
Jeevithan Mahenthran <JeevithanMahenthran@gmail.com>
2024-03-26 00:49:44 -0700

    fixed icon and added lvel from local storage

--- front-end/stats.js
+++ front-end/stats.js
@@ -1,16 +1,14 @@
-const iconURL = 'https://ddragon.leagueoflegends.com/cdn/14.6.1/img/profileicon/';
 var summonerName = localStorage["playerName"];
 var summonerTag = localStorage["playerTag"];
+var summonerLevel = localStorage["sumLevel"];
 var playerIcon = localStorage["iconID"];
 // var playerLevel = 150; //Hard Set to Lvl 150 for testin
-
-
-var summonerIconURL = iconURL + playerIcon + ".png";
+var summonerIconURL = 'https://ddragon.leagueoflegends.com/cdn/14.6.1/img/profileicon/' + playerIcon + ".png"; // using datadragon to get icon from iconID
 
 document.querySelector('#playerInfo').innerHTML = "<h2>" + summonerName + "#"+ summonerTag + "</h2>"; 
 document.querySelector('#playerIcon').innerHTML = "<img src = \"" + summonerIconURL + "\">";
 
 // console.log("URL", summonerIconURL);
-// document.querySelector('#playerInfo').innerHTML += "<p id = \"playerLvl\"> Level:" + playerLevel + "<p>";
-// document.querySelector('#playerTag').innerHTML = "<h2> #" + summonerTag + "</h2>";
+document.querySelector('#playerInfo').innerHTML += "<p id = \"playerLvl\"> Level: " + summonerLevel + "<p>";
+document.querySelector('#playerTag').innerHTML = "<h2> #" + summonerTag + "</h2>";
 // document.querySelector('#playerIcon').innerHTML = "<img src = \"" + playerIcon + "\" alt = \"Player Icon's not loading\">";
