35be26b5070d0bfcd3850d1e5ca162d8e35be448
Jeevithan Mahenthran <JeevithanMahenthran@gmail.com>
2024-03-26 00:50:25 -0700

    added some comments

--- front-end/stats.js
+++ front-end/stats.js
@@ -5,10 +5,9 @@ var playerIcon = localStorage["iconID"];
 // var playerLevel = 150; //Hard Set to Lvl 150 for testin
 var summonerIconURL = 'https://ddragon.leagueoflegends.com/cdn/14.6.1/img/profileicon/' + playerIcon + ".png"; // using datadragon to get icon from iconID
 
+// data getting passed to website
 document.querySelector('#playerInfo').innerHTML = "<h2>" + summonerName + "#"+ summonerTag + "</h2>"; 
 document.querySelector('#playerIcon').innerHTML = "<img src = \"" + summonerIconURL + "\">";
-
-// console.log("URL", summonerIconURL);
 document.querySelector('#playerInfo').innerHTML += "<p id = \"playerLvl\"> Level: " + summonerLevel + "<p>";
 document.querySelector('#playerTag').innerHTML = "<h2> #" + summonerTag + "</h2>";
 // document.querySelector('#playerIcon').innerHTML = "<img src = \"" + playerIcon + "\" alt = \"Player Icon's not loading\">";
