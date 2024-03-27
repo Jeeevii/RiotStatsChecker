443b4bac3617a39c1044af37b7b04c7581016941
Jeevithan Mahenthran <JeevithanMahenthran@gmail.com>
2024-03-26 21:08:15 -0700

    added new library for png's, added top 3 champs

--- front-end/stats.js
+++ front-end/stats.js
@@ -12,11 +12,15 @@ var soloLP = localStorage["soloLP"];
 var flexTier = localStorage["flexTier"];
 var flexRank = localStorage["flexRank"];
 var flexLP = localStorage["flexLP"];
-
+// top 3 most played champions data 
+var champ1_ID = localStorage["champ1_ID"]
+var champ2_ID = localStorage["champ2_ID"]
+var champ3_ID = localStorage["champ3_ID"]
 
 // =============================================================================================================================================================
 // using parsed data and presenting them in a cooler way
 var summonerIconURL = 'https://ddragon.leagueoflegends.com/cdn/14.6.1/img/profileicon/' + playerIcon + ".png"; // using datadragon to get icon from iconID
+var championIconURL = 'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/'; // add champtionID.png
 
 // image and rank data for solo
 document.querySelector('#soloTier').textContent = "Ranked Solo " + soloTier + " " + soloRank + " " + soloLP + " LP";
@@ -24,7 +28,6 @@ const soloImg = document.createElement('img');
 soloImg.src = 'Ranked Emblems/' + soloTier + '.png';
 soloImg.style.width = '20%';
 soloImg.style.height = '20%'; 
-
 // image and rank data for flex 
 document.querySelector('#flexTier').textContent = "Ranked Flex " + flexTier + " " + flexRank + " " + flexLP + " LP";
 const flexImg = document.createElement('img');
@@ -33,26 +36,32 @@ flexImg.style.width = '20%';
 flexImg.style.height = '20%'; 
 
 
-// =============================================================================================================================
+// ================================================================================================================================================================
 // data getting passed to website
-document.querySelector('#playerInfo').innerHTML = "<h2>" + summonerName + " #"+ summonerTag + "</h2>"; // player name and tag
-document.querySelector('#playerIcon').innerHTML = "<img src = \"" + summonerIconURL + "\">"; // player icon 
-document.querySelector('#playerInfo').innerHTML += "<p id = \"playerLvl\"> Level: " + summonerLevel + "<p>"; // player level 
 
-// solo tier
+// solo tier edge case
 if (soloTier == "N/A"){
     soloImg.src = 'Ranked Emblems/UNRANKED.png';
     soloImg.style.width = '40%';
     soloImg.style.height = '40%';   
 }
 document.querySelector('#soloTier').appendChild(soloImg);
-// flex tier 
+// flex tier edge case
 if (flexTier == "N/A"){
     flexImg.src = 'Ranked Emblems/UNRANKED.png';
     flexImg.style.width = '40%';
     flexImg.style.height = '40%';   
 }
-document.querySelector('#flexTier').appendChild(flexImg);
+
+document.querySelector('#playerInfo').innerHTML = "<h2>" + summonerName + " #"+ summonerTag + "</h2>"; // player name and tag
+document.querySelector('#playerIcon').innerHTML = "<img src = \"" + summonerIconURL + "\">"; // player icon 
+document.querySelector('#playerInfo').innerHTML += "<p id = \"playerLvl\"> Level: " + summonerLevel + "<p>"; // player level 
+document.querySelector('#soloTier').appendChild(soloImg); // uploading solo rank
+document.querySelector('#flexTier').appendChild(flexImg); // uploading flex rank 
+document.querySelector('#champ1').innerHTML = "<img src='" + championIconURL + champ1_ID + ".png'>"; // top 3 champs icons
+document.querySelector('#champ2').innerHTML = "<img src='" + championIconURL + champ2_ID + ".png'>";
+document.querySelector('#champ3').innerHTML = "<img src='" + championIconURL + champ3_ID + ".png'>";
+
 
 
 
