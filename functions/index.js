f787328bbe43ae1bf30e64c7f2e1c164a26cbef3
alexlee39 <alexanderlee014@gmail.com>
2024-03-29 22:50:44 -0700

    Updated more firebase code

--- functions/index.js
+++ functions/index.js
@@ -6,23 +6,22 @@ const { spawn } = require('child_process');
 const { takeCoverage } = require('v8');
 const app = express();
 const port = 3000;
+const api_key = 'RGAPI-a20a9e41-4a88-448b-ab94-20aef94eabac';
+// const na
 
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.get('/', (req,res) =>{
-//     res.status(200).send("<h1>test</h1>");
-// });
 app.get('/client-search', async (req, res) => {
     // receiving data from script.js
     const summonerName = req.query.summonerName;
     const summonerTag = req.query.summonerTag;
     console.log(summonerName, summonerTag);
     let data = await getPuuid(summonerName,summonerTag);
-    console.log("server side funct running");
-    console.log(data);
-    console.log(data['puuid']);
+    // console.log("server side funct running");
+    // console.log(data);
+    // console.log(data['puuid']);
     res.status(200).send(data['puuid'].toString());
     
     //python code: --> Doesn't work with firebase hosting 
@@ -42,24 +41,10 @@ app.get('/client-search', async (req, res) => {
 });
  
 async function getPuuid(sName, sTag){
-    const data = await fetch(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${sName}/${sTag}?api_key=RGAPI-a20a9e41-4a88-448b-ab94-20aef94eabac`);
-    console.log("GetPuuid funct running!");
-    // console.log(data['puuid']);
-    // console.log(data);
+    const data = await fetch(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${sName}/${sTag}?api_key=${api_key}`);
     let out = await data.json()
     console.log(out);
     return out;
 }
-// app.post('/client-search', (req,res) =>{
-//     const riotNameAndTag = req.body;
-//     if(!riotNameAndTag){
-//         return res.status(400).send({status: failed})
-//     }
-//     res.status(200).send()
-// });
-
-// app.listen(port, () => {
-//     console.log(`Server running at http://localhost:${port}`);
-// });
 
 exports.app = functions.https.onRequest(app);
--- public/script.js
+++ public/script.js
@@ -20,7 +20,9 @@ document.getElementById('searchButton').addEventListener('click', async function
 
     const url = `/client-search?summonerName=${encodeURIComponent(summonerName)}&summonerTag=${encodeURIComponent(summonerTag)}`; // location of httpserver.js
     console.log("(HTML) Clicked! Sending and Receiving Data Now...");
-    const response = await fetch(localBaseURL+url);
+    const apiCallURL = publicBaseURL+url;
+    console.log(apiCallURL);
+    const response = await fetch(apiCallURL);
 
         // {
         //     method: 'POST'
@@ -48,7 +50,7 @@ document.getElementById('searchButton').addEventListener('click', async function
     // console.log("(HTML) Logging data from JavaScript into website console:");
 
     // // console.log("(HTML) parsed data from Python: ", userName, tagLine, iconID, sumLevel, soloTier, soloRank, soloLP, flexTier, flexRank, flexLP, champ1_ID, champ1_mastery, champ2_ID, champ2_mastery, champ3_ID, champ3_mastery); 
-    // // document.getElementById('tempOutput').innerText = data; // displaying the returned data on front end
+    document.getElementById('tempOutput').innerText = data; // displaying the returned data on front end
 
     // //Switch page at the end of getting data... 
     // changePage('stats.html', parsedData);
